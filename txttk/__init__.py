21fd733af78bd402c3b568927b354abfa3bad7c4
jeroyang <jeroyang@gmail.com>
2016-07-12 20:22:16 +0800

    simplify import

--- txttk/__init__.py
+++ txttk/__init__.py
@@ -5,6 +5,9 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import *
 
+from txttk import nlptools, retools, feature
+
+__all__ = ['feature', 'nlptools', 'retools']
 __author__ = 'Chia-Jung, Yang'
 __email__ = 'jeroyang@gmail.com'
 
