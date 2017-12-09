8e6daf9cbb7dfbc4900870fb365add17929bd4ab
jeroyang <jeroyang@gmail.com>
2017-12-09 19:43:02 +0800

    Bump version: 0.10.8 → 0.11.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='txttk',
-    version='0.10.8',
+    version='0.11.0',
     description="Text Toolkit: manipulate text & regular expression",
     long_description=readme,
     author="Chia-Jung, Yang",
